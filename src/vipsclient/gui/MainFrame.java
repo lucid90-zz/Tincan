04606a9667c95d0f440c06c01c2d78d0f2c71ac7
Lucian Dobre <lucian.dobre.1337@gmail.com>
2014-06-19 10:36:13 +0300

    The start. Just a frame.

---
+++ src/vipsclient/gui/MainFrame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vipsclient.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author LucianDobre
+ */
+public class MainFrame extends JFrame{
+
+    public MainFrame(){
+        setTitle("Vips");
+        setSize(600, 400);
+        setVisible(true);
+    }
+    
+}
